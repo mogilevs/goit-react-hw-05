148ff47262f4fb2781117aa78fb61ec856f77330
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-08-25 21:59:57 +0300

    do trending movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
+import clsx from "clsx";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import HomePage from "./pages/HomePage";
@@ -8,16 +9,54 @@ import MovieDetailsPage from "./pages/MovieDetailsPage";
 import MovieCast from "./components/MovieCast/MovieCast";
 import MovieReviews from "./components/MovieReviews/MovieReviews";
 import NotFoundPage from "./pages/NotFoundPage";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const buildLinkClass = ({ isActive }) => {
+    return clsx("link", isActive && "active");
+  };
+  const url =
+    "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
+  const imageUrl = "https://image.tmdb.org/t/p/w500";
+  const options = {
+    headers: {
+      // Замість api_read_access_token вставте свій токен
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OWZlYTc2MGYwYjNiMDA1MzdjZjUxNjAyOThlYzYwMCIsIm5iZiI6MTcyNDYwOTI5MC42MzUyNDIsInN1YiI6IjY2Y2IzZTczN2MxYWUyNGEzMDBmOGJhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xPLJpJDs-V4d_S27LEUpu9knTECN5dmXgeU3U2N0mMo",
+    },
+  };
+
+  async function trendingMoviesRequest() {
+    await axios
+      .get("https://api.themoviedb.org/3/trending/movie/day", options)
+      .then((response) => setTrendingMovies(response.data.results))
+      .catch((err) => console.error(err));
+  }
+  trendingMoviesRequest();
 
   return (
     <>
       <Navigation>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/movies">Movies</NavLink>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/movies" className={buildLinkClass}>
+          Movies
+        </NavLink>
       </Navigation>
+      <h2>Trending today</h2>
+      <ul>
+        {trendingMovies.map(({ id, title }) => {
+          return (
+            <li key={id}>
+              <a href={`/movies/:${id}`} alt={title}>
+                {title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,1 +1,4 @@
-export default function Navigation() {}
+import css from "./Navigation.module.css";
+export default function Navigation({ children }) {
+  return <nav className={css.nav}>{children}</nav>;
+}
